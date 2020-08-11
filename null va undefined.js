21358185a79562f9584ffc0a649c7a941db8f8b4
SinhaEtu <sinha.diu@gmail.com>
2020-08-11 20:44:20 +0600

    first commit

---
+++ null va undefined.js
@@ -0,0 +1,27 @@
+//undefined
+
+
+let pakhi;
+console.log(pakhi);
+
+function add(num1,num2){
+    console.log(num1+num2);
+    return;
+}
+const result = add(13,82);
+console.log(result);
+
+function add (num1,num2){
+    console.log(num2);
+}
+const result = add(13);
+console.log(result);
+
+const premik = {name:"sin",phone:09876};
+console.log(premik.gf);
+
+let fun = undefined;
+console.log(fun);
+
+let agaes = [2,5,7];
+console.log(ages[55]);
